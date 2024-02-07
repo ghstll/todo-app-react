251a10e5d232ba9e8bd180ac0c667799360df90e
ghstll <129940439+ghstll@users.noreply.github.com>
2024-02-06 20:20:40 -0600

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './Button'
+export function App(){
+    return (
+        <aside>
+            <header>
+                <h1>Inicio</h1>
+            </header>
+            <Button text="Mi boton"></Button>
+            <Button text="Mi boton"></Button>
+            <Button text="Mi boton"></Button>
+            <Button text="Mi boton"></Button>
+            <Button text="Mi boton"></Button>
+        </aside>
+    )
+}
